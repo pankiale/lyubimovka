69118d014a31a7a3cf4c14a02ade91f63290439d
Serebrennikovki <serebrennikov_k_I@mail.ru>
2022-06-10 23:03:31 +0300

    add slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,3 +16,21 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
+
+  const swiperReview = new Swiper('.swiperReview',{
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.reviews__slider-feedback-pagination',
+      clickable: true,
+
+    },
+
+    navigation: {
+      nextEl: '.reviews__slider-feedback-button-next',
+      prevEl: '.reviews__slider-feedback-button-prev',
+    },
+  })
