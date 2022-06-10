2bfaa10e51a7f7018c9c45b8e43d831ddc849ca0
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2022-06-10 23:18:12 +0300

    количество слайдов адаптировано под разрешение

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,70 +1,94 @@
 // ---------------------------------------------------- constants
-const burger = document.querySelector('.burger');
-const menuPopup = document.querySelector('.popup');
-const page = document.querySelector('.page');
+const burger = document.querySelector(".burger");
+const menuPopup = document.querySelector(".popup");
+const page = document.querySelector(".page");
 
 // ---------------------------------------------------- functions
 function openPopup(popup) {
-    popup.classList.add('popup_opened');
+    popup.classList.add("popup_opened");
     addCloseListeners();
 }
 
 function closePopup(popup) {
-    popup.classList.remove('popup_opened');
+    popup.classList.remove("popup_opened");
     deleteCloseListeners();
 }
 
 function setCloseListeners() {
-    const popups = document.querySelectorAll('.popup')
+    const popups = document.querySelectorAll(".popup");
     popups.forEach((popup) => {
-        popup.addEventListener('mousedown', (evt) => {
-            if (evt.target.classList.contains('popup_opened')) {
-                closePopup(popup)
+        popup.addEventListener("mousedown", (evt) => {
+            if (evt.target.classList.contains("popup_opened")) {
+                closePopup(popup);
             }
-            if (evt.target.classList.contains('popup__close-button')) {
-                closePopup(popup)
+            if (evt.target.classList.contains("popup__close-button")) {
+                closePopup(popup);
             }
-        })
-    })
+        });
+    });
 }
 
 const handleEscape = (evt) => {
-    if (evt.key === 'Escape') {
-        const openedPopup = document.querySelector('.popup_opened');
+    if (evt.key === "Escape") {
+        const openedPopup = document.querySelector(".popup_opened");
         closePopup(openedPopup);
     }
-}
+};
 
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
+const swiperFeedback = new Swiper(".swiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    direction: 'horizontal',
-    loop: true,
+    direction: "horizontal",
+    loop: false,
 
     pagination: {
-        el: '.reviews__slider-feedback-pagination',
+        el: ".reviews__slider-feedback-pagination",
         clickable: true,
-
     },
 
     navigation: {
-        nextEl: '.reviews__slider-feedback-button-next',
-        prevEl: '.reviews__slider-feedback-button-prev',
+        nextEl: ".reviews__slider-feedback-button-next",
+        prevEl: ".reviews__slider-feedback-button-prev",
     },
 
+    breakpoints: {
+        930: {
+            slidesPerView: 2,
+        },
+
+        1321: {
+            slidesPerView: 3,
+        },
+    },
 });
+
+// const buttonPag = document.querySelector(".button");
+// const pag = document.querySelector(".reviews__slider-feedback-pagination-mob");
+// const newPag = document.querySelector(".reviews__slider-feedback-pagination");
+// buttonPag.addEventListener("click", () => {
+//     console.log(swiperFeedback.pagination.el, "123");
+// });
+// window.addEventListener("resize", () => {
+//     if (window.screen.width >= 930) {
+//         console.log("newPag");
+//         swiperFeedback.pagination.el = newPag;
+//     } else {
+//         console.log("pag");
+//         swiperFeedback.pagination.el = pag;
+//     }
+// });
 // ---------------------------------------------------- listeners
-burger.addEventListener('click', () => {
+burger.addEventListener("click", () => {
     openPopup(menuPopup);
-})
+});
 
 const addCloseListeners = () => {
-    page.addEventListener('keydown', handleEscape);
-}
+    page.addEventListener("keydown", handleEscape);
+};
 
 const deleteCloseListeners = () => {
-    page.removeEventListener('keydown', handleEscape);
-}
+    page.removeEventListener("keydown", handleEscape);
+};
 // ---------------------------------------------------- execution
 
 setCloseListeners();
